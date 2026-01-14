dea75ca24d763d64f50c58392a8caec843611082
Lukas Breiling <lukas.breiling@crpusd.org>
2026-01-14 08:46:18 -0800

    Customizing the ping button, making the save button, adding an automatic url for the curl function

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -19,22 +19,44 @@ def do_command(command):
 
     # If url_entry is blank, use localhost IP address 
     url_val = url_entry.get()
+    
+    # Makes curl command automatically have a url
+    if command == "curl":
+        url_val = "http://wttr.in/"
+        
     if (len(url_val) == 0):
         # url_val = "127.0.0.1"
         url_val = "::1"
-
+    
     with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
         for line in p.stdout:
             command_textbox.insert(tk.END,line)
             command_textbox.update()
     
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
+
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
 # Makes the command button pass it's name to a function using lambda
-ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping"))
-ping_btn.pack()
+ping_btn = tk.Button(frame, text="Ping", 
+    command=lambda:do_command("ping"),
+    compound="center",
+    font=("comic sans", 12),
+    foreground=("white"),
+    bd=0, 
+    relief="flat",
+    bg="black", activebackground="gray")
+ping_btn.pack() 
 
 # Sets up command button for tracert
 tracert = tk.Button(frame, text="tracert", command=lambda:do_command("tracert"))
@@ -70,6 +92,9 @@ url_label = tk.Label(frame_URL, text="Enter a URL of interest: ",
 url_label.pack(side=tk.LEFT)
 url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
 url_entry.pack(side=tk.LEFT)
+
+save = tk.Button(frame, text="save", command=lambda:mSave())
+save.pack()
 
 frame = tk.Frame(root,  bg="black") # change frame color
 frame.pack()
