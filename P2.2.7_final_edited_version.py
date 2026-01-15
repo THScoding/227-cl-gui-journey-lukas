2719af9b1942b990625886ae7c2d3d8b31d683b3
Lukas Breiling <lukas.breiling@crpusd.org>
2026-01-15 10:08:20 -0800

    Making The List Var Kinda work, not done yet

--- P2.2.7_final_edited_version.py
+++ P2.2.7_final_edited_version.py
@@ -4,11 +4,15 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command(command):
+def do_command():
     global output_widget
+    global commandslistindex
+    
+    commandslistindex = commandslistindecies[0]
+    curcommand = Command_ListBox.get(commandslistindex)
     
     output_widget.delete(1.0, tk.END)
-    output_widget.insert(tk.END, command + " working....\n")
+    output_widget.insert(tk.END, curcommand + " working....\n")
     output_widget.update()
     
     global url_entry
@@ -17,25 +21,38 @@ def do_command(command):
     url_val = url_entry.get()
     
     # Makes curl command automatically have a url
-    if command == "curl":
+    if curcommand == "curl":
         url_val = "http://wttr.in/"
         
     if (len(url_val) == 0):
         # url_val = "127.0.0.1"
         url_val = "::1"
     
-    with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+    with subprocess.Popen(curcommand + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
         for line in p.stdout:
             output_widget.insert(tk.END,line)
             output_widget.update()
             
 MAIN_WINDOW= tk.Tk()
+
+# Variable Configuration
+commandslist = ["ping", "tracert", "netstat", "curl", "start", "nslookup"]
+commandslistvar = tk.Variable(value=commandslist)
+
+########
+
 Command_selection=tk.Frame(MAIN_WINDOW, height=200, width= 50)
 Command_selection.pack()
 
-Command_ListBox = tk.Listbox(Command_selection, listvariable= )
+Command_ListBox = tk.Listbox(Command_selection, listvariable= commandslistvar, height= 5)
+Command_ListBox.pack()
 
+commandslistindecies = Command_ListBox.curselection()
 
+startbutton = tk.Button(Command_selection, text = "START", command=lambda:do_command())
+startbutton.pack()
+
+#######
 
 Command_entries= tk.Frame(MAIN_WINDOW)
 Command_entries.pack()
@@ -45,3 +62,5 @@ output.pack()
 
 output_widget=tksc.ScrolledText(output,height=10,width=100)
 output_widget.pack()
+
+MAIN_WINDOW.mainloop()
